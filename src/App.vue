93a3d04f2fc86722df06fde2f0954d3b44f9649d
rifkigantengsangat <rmrifki7448@gmail.com>
2022-10-04 19:12:25 +0700

    fixing bug

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import NavbarView from './components/NavbarView.vue';
 </script>
 
 <template>
-<div class="hidden md:block  w-full h-12">
+<div class="h-0 w-full md:h-12">
   <NavbarView/>
 </div>
   <RouterView />
